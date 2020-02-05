c32a02e199a0ee0fe7bb76a46a2ac576f7cda551
Andika Nugraha <andika.nugraha@detik.com>
2020-02-05 16:34:25 +0700

    model

--- app/model/dev.go
+++ app/model/dev.go
@@ -1,2 +1,6 @@
 package model
 
+type Dev struct {
+	ID		int64 `json:"id"`
+	Name 	string `json:"name"`
+}
--- app/repository/repository.go
+++ app/repository/repository.go
@@ -2,8 +2,9 @@ package repository
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/andikanugraha11/Golang-Boilerplate-awesome-echo/app/model"
 )
 
 type DevRepo interface {
-	Fetch(c echo.Context, num int64) (int, error)
+	Fetch(c echo.Context, num int64) (*model., error)
 }
